f502fb1274d3bcdc3fc6a8ea1d7ad5e55ae17ff4
Joaosvc <joaosvc.dev@gmail.com>
2023-08-08 01:20:06 -0300

    :sparkless: Adding somes changes

--- src/app/functions/extract.tsx
+++ src/app/functions/extract.tsx
@@ -1,27 +1,16 @@
 import JSZip from 'jszip'
-import fs from 'fs' // Importar o módulo de sistema de arquivos do Node.js
 
 export async function ExtractFiles(file: File) {
-  const zip = new JSZip()
-  console.log('Chegou aqui', await file.arrayBuffer())
-
   try {
     const zipData = await file.arrayBuffer()
-    await zip.loadAsync(zipData)
 
-    const outputFolderPath = '../../' // Pasta de saída para salvar os arquivos extraídos
+    await JSZip.loadAsync(file).then((zip: JSZip) => {
+      Object.keys(zip.files).forEach((filename) => {
+        let content = zip.file(filename)?.async('string')
+        let newFilename = filename.replace(' ', '_')
 
-    if (!fs.existsSync(outputFolderPath)) {
-      fs.mkdirSync(outputFolderPath) // Cria a pasta de saída se ela não existir
-    }
-
-    zip.forEach(async (relativePath, zipEntry) => {
-      if (!zipEntry.dir) {
-        const content = await zipEntry.async('uint8array')
-        const outputPath = `${outputFolderPath}/${relativePath}`
-
-        fs.writeFileSync(outputPath, Buffer.from(content)) // Salva o conteúdo extraído no arquivo
-      }
+        console.log(content, newFilename)
+      })
     })
   } catch (error) {
     console.error('Erro:', error)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
+      <head>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+      </head>
       <body className={poppins.className}>{children}</body>
     </html>
   )
